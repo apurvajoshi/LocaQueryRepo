00e8e5816d0bba75ca573d91ade59553d796129a
Apurva Joshi <apurvaj@andrew.cmu.edu>
2013-04-18 17:23:42 -0400

    Ignoring local files

--- locaQuery/locaQueryAppDelegate.m
+++ locaQuery/locaQueryAppDelegate.m
@@ -19,7 +19,22 @@
     self.viewController = [[locaQueryViewController alloc] initWithNibName:@"locaQueryViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+    
+    // Let the device know we want to receive push notifications
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
     return YES;
+}
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", error);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
